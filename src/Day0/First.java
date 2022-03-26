08079f16c8f0b4acc69d86a755effaaeb6425026
persik <diana.bulgac@gmail.com>
2022-03-26 16:07:03 -0400

    updated first file

--- src/Day0/First.java
+++ src/Day0/First.java
@@ -3,5 +3,8 @@ package Day0;
 public class First {
 
     //TODO: work on this tommorow //add some cool code
+    //more comments that we need to commit at some point
+    //extra line 1
+    //extra lan2
 
 }
